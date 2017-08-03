b1c027ae648a6a9f39b31ad8f93b4b205ec468cf
yogeshsahuu <30623253+yogeshsahuu@users.noreply.github.com>
2017-08-03 15:46:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/artical-one', function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
+    res.sendFile(path.join(__dirname,  'artical-one.html'));
 });
 
 app.get('/artical-two', function(req,res){
