ce097469e42cc6ac0a32e88d81509cda1eb9d23f
yogeshsahuu <30623253+yogeshsahuu@users.noreply.github.com>
2017-08-07 11:42:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,82 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var AricalOne = { 
+    title : 'Artical-One | Yogesh Sahu',
+    heading : 'Artical-One',
+    date: 'Aug 3 2017',
+    content: `
+             <p>
+                Hello I am Yogesh from bhopal thia is artical one for thi app to be very cleare it is not tha time for going to the plase like that.
+            </p>
+            
+            <p>
+                Hello I am Yogesh from bhopal thia is artical one for thi app to be very cleare it is not tha time for going to the plase like that.
+            </p>
+            
+            <p>
+                Hello I am Yogesh from bhopal thia is artical one for thi app to be very cleare it is not tha time for going to the plase like that.
+            </p> `
+    
+};
+function createHtml (data){
+    var title = data.title;
+    var date = data.date;
+    var content = data.content;
+    var heading = data.heading;
+    
+
+var htmlTempleate = `
+    
+    <html>
+<head>
+    
+    <title>
+        $(title)
+    </title>
+    
+    
+    <link href="/ui/style.css" rel="stylesheet" />
+    
+   
+     </head>
+    
+    <body>
+        <div  class = "container">
+        
+        <div>
+         <a href="/"> Home</a>
+        </div>
+        <hr/>
+        
+        <h3>
+            $(heading)
+        </h3>
+        <div>
+            $(date)
+        </div>
+        <div>
+            
+            $(content)
+            
+        </div>
+        </div>
+    </body>
+    
+
+
+</html>
+`;
+return htmlTempleate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/artical-one', function (req, res){
-    res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
+    res.send(createHtml(AricalOne));
 });
 
 app.get('/artical-two', function(req,res){
