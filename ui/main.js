7048067033b3d1feafe52dbdeef51bc85d2c9a3f
yogeshsahuu <30623253+yogeshsahuu@users.noreply.github.com>
2017-08-16 15:37:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -44,7 +44,7 @@ submit.onclick =  function(){
          
      }
      
-     var ul= document.getElementById('nameList');
+     var ul= document.getElementById('namelist');
      ul.innerHTML = list;
      
      
