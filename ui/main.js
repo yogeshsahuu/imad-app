9da873b04c8c2a91215ddc0c2462e49cd861efdf
yogeshsahuu <30623253+yogeshsahuu@users.noreply.github.com>
2017-08-16 15:54:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,7 +40,7 @@ submit.onclick =  function(){
    var list = '';
      for(i=0; i < names.length; i++)
      {
-         list = '<li>'+ names[i] +'</li>';
+         list == '<li>'+ names[i] +'</li>';
          
      }
      
